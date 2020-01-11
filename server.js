ab1d51cbe64dc99c07e7b2f8dd8f33cc0c4b3585
Charles Wallace <wallacecs007@gmail.com>
2020-01-11 12:10:30 -0700

    Server configured

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 
+const PRouter = require('./project/prouter.js')
+const TRouter = require('./task/trouter.js')
+const RRouter = require('./resource/rrouter.js')
+
 const server = express();
 server.use(express.json());
 
+server.use('/projects', PRouter)
+server.use('/resources', RRouter)
+server.use('/tasks', TRouter)
 
 module.exports = server;
